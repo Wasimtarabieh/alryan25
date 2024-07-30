80db69ba18fa22ac82e9ac2316bb036390b1cfd3
EduVantage-24 <153000293+Wasimtarabieh@users.noreply.github.com>
2024-07-30 19:37:44 +0300

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,35 +1,15 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const serviceID = 'service_6oului3';
-    const templateID = 'template_577zftz';
-
-    const formData = {
-        student_id: document.getElementById('student-id').value,
-        branch: document.getElementById('branch').value,
-        first_name: document.getElementById('first-name').value,
-        last_name: document.getElementById('last-name').value,
-        id_number: document.getElementById('id-number').value,
-        dob: document.getElementById('dob').value,
-        workplace: document.getElementById('workplace').value,
-        address: document.getElementById('address').value,
-        email: document.getElementById('email').value,
-        mobile_phone: document.getElementById('mobile-phone').value,
-        landline_phone: document.getElementById('landline-phone').value,
-        education: document.getElementById('education').value,
-        referrer: document.getElementById('referrer').value,
-        course_start_date: document.getElementById('course-start-date').value,
-        course_name: document.getElementById('course-name').value,
-        tuition: document.getElementById('tuition').value,
-        registration_fee: document.getElementById('registration-fee').value,
-        total_fee: document.getElementById('total-fee').value,
-        payment_method: document.getElementById('payment-method').value
-    };
-
-    emailjs.send(serviceID, templateID, formData)
-        .then(response => {
+    
+    const form = event.target;
+    const formData = new FormData(form);
+    const formObject = Object.fromEntries(formData.entries());
+    
+    emailjs.send('service_6oului3', 'template_577zftz', formObject)
+        .then(function(response) {
             alert('تم إرسال النموذج بنجاح!');
-        }, error => {
-            alert('فشل في إرسال النموذج، الرجاء المحاولة مرة أخرى.');
+            form.reset();
+        }, function(error) {
+            alert('حدث خطأ أثناء إرسال النموذج، الرجاء المحاولة مرة أخرى.');
         });
 });
